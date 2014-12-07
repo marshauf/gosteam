956b7bd368987f5cdbe3806f50e75ae861868153
Marcel Hauf <mail@marcelhauf.name>
2014-12-07 16:28:04 +0100

    Add UpToDateCheck.RequiredVersion field

--- uptodatecheck.go
+++ uptodatecheck.go
@@ -13,11 +13,13 @@ type upToDateCheckResponse struct {
 type UpToDateCheckResponse struct {
 	Success           bool   `json:"success"`
 	UpToDate          bool   `json:"up_to_date"`
+	RequiredVersion   int    `json:"required_version"`
 	VersionIsListable bool   `json:"version_is_listable"`
 	Message           string `json:"message"`
 }
 
 func UpToDateCheck(appid, version int) (*UpToDateCheckResponse, error) {
+	// http://api.steampowered.com/ISteamApps/UpToDateCheck/v0001/?appid=570&version=36
 	url := "http://api.steampowered.com/ISteamApps/UpToDateCheck/v0001/?appid=" + strconv.Itoa(appid) + "&version=" + strconv.Itoa(version)
 	resp, err := http.Get(url)
 	if err != nil {
